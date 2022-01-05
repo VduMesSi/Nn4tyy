24cb085c55bd2b299401c65bb0b4bc58c2d40aa8
VduMesSi <95524908+VduMesSi@users.noreply.github.com>
2022-01-05 19:24:51 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,8 +36,7 @@ cat << EOF > ${DIR_TMP}/flym.json
 EOF
 
 # Get V2Ray executable release
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
-busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 
 # Convert to protobuf format configuration
 mkdir -p ${DIR_CONFIG}
