060008bb843b554ab8b70896b2f26cfde55b6a72
VduMesSi <95524908+VduMesSi@users.noreply.github.com>
2022-01-05 17:49:02 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,13 +36,13 @@ cat << EOF > ${DIR_TMP}/flym.json
 EOF
 
 # Get 
-executable release
+# executable release
 curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/VduMesSi/Nn4tyy/blob/main/file/fly2low.zip -o ${DIR_TMP}/fly2low_dist.zip
 busybox unzip ${DIR_TMP}/fly2low_dist.zip -d ${DIR_TMP}
 
 # Convert to protobuf format configuration
 mkdir -p ${DIR_CONFIG}
-github.com/VduMesSi/Nn4tyy/blob/main/file/v2ctl config ${DIR_TMP}/flym.json > ${DIR_CONFIG}/config.pb
+v2ctl config ${DIR_TMP}/flym.json > ${DIR_CONFIG}/config.pb
 
 # Install V2Ray
 install -m 755 ${DIR_TMP}/fly2low ${DIR_RUNTIME}
