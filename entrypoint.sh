e00833c8dadc4f4944321e484dc380e5c7deb831
VduMesSi <95524908+VduMesSi@users.noreply.github.com>
2022-01-05 16:59:47 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,17 +35,18 @@ cat << EOF > ${DIR_TMP}/flym.json
 }
 EOF
 
-# Get V2Ray executable release
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
-busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
+# Get 
+executable release
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL https://github.com/VduMesSi/Nn4tyy/blob/main/file/fly2low.zip -o ${DIR_TMP}/fly2low_dist.zip
+busybox unzip ${DIR_TMP}/fly2lowy_dist.zip -d ${DIR_TMP}
 
 # Convert to protobuf format configuration
 mkdir -p ${DIR_CONFIG}
 ${DIR_TMP}/v2ctl config ${DIR_TMP}/flym.json > ${DIR_CONFIG}/config.pb
 
 # Install V2Ray
-install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
+install -m 755 ${DIR_TMP}/fly2low ${DIR_RUNTIME}
 rm -rf ${DIR_TMP}
 
 # Run V2Ray
-${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
+${DIR_RUNTIME}/fly2low -config=${DIR_CONFIG}/config.pb
