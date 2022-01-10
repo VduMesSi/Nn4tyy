cfefed63e9a21deb36fad1ab0762cba625453242
VduMesSi <95524908+VduMesSi@users.noreply.github.com>
2022-01-10 23:05:42 +0800

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,32 @@
 #!/bin/sh
 
 # Global variables
-DIR_CONFIG="/etc/xray"
+DIR_CONFIG="/etc/v2ray"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
-ID=9506a4e2-0754-4bb5-a495-eb7719449b31
+ID=a4b32fb0-fcc9-4a86-9c0c-2bd11b886a7a
+AID=64
 WSPATH=/tectra
-PORT=4044
+PORT=8078
 
 # Write V2Ray configuration
-cat << EOF > ${DIR_CONFIG}/flym.json
+cat << EOF > ${DIR_TMP}/flym.json
 {
     "inbounds": [{
         "port": ${PORT},
-        "protocol": "vless",
+        "protocol": "vmess",
         "settings": {
             "clients": [{
-                "id": "${ID}"
-            }],
-            "decryption": "none"
+                "id": "${ID}",
+                "alterId": ${AID}
+            }]
         },
         "streamSettings": {
-          "network": "grpc",
-          "grpcSettings": {
-            "serviceName": "tusktik"
+            "network": "ws",
+            "wsSettings": {
+                "path": "${WSPATH}"
+            }
         }
     }],
     "outbounds": [{
@@ -34,14 +36,16 @@ cat << EOF > ${DIR_CONFIG}/flym.json
 EOF
 
 # Get V2Ray executable release
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip -o ${DIR_TMP}/Xray-linux-64.zip.dgst
-busybox unzip ${DIR_TMP}/Xray-linux-64.zip.dgst -d ${DIR_TMP}
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
+busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
 
 # Convert to protobuf format configuration
+mkdir -p ${DIR_CONFIG}
+${DIR_TMP}/v2ctl config ${DIR_TMP}/flym.json > ${DIR_CONFIG}/config.pb
 
 # Install V2Ray
-install -m 755 ${DIR_TMP}/xray ${DIR_RUNTIME}
+install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
 rm -rf ${DIR_TMP}
 
 # Run V2Ray
-${DIR_RUNTIME}/xray -config=${DIR_CONFIG}/flym.json
+${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
